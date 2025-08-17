d9a79876dbb41e055c4fb71fb2b28c62a4501b12
toobashahid210 <toobashahid210@gmail.com>
2025-08-17 19:07:57 +0500

    env setup

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# activate the virtual environment
+source venv/Scripts/activate
+
 # Run FastAPI app (on port 8000)
-uvicorn app2:app --reload --host 0.0.0.0 --port 8000 &
+uvicorn app:app --reload --host 0.0.0.0 --port 8000 &
 
 # Run Streamlit app (on port 8501)
-streamlit run streamlit_app.py --server.port 8501 &
+streamlit run ui.py --server.port 8501 &
 
 # Wait for both to finish
 wait
